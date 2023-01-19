a7334c703c53907e39830a7cc5992f92715560cc
Lucy Lee <55265883+lucylee418@users.noreply.github.com>
2023-01-19 09:44:14 -0500

    Updated

---
+++ LC1480.java
@@ -0,0 +1,21 @@
+public class LC1480 {
+
+    public int[] runningSum(int[] nums) {
+        int[] mySum = new int[nums.length];
+        int currSum = 0;
+        for (int i=0; i<nums.length; i++){
+            currSum += nums[i];
+            mySum[i] = currSum;
+        }
+        return mySum;
+    }
+
+
+    public static void main (String[] args){
+        LC1480 lc1480 = new LC1480();
+        int[] myArray = {1,2,3,4};
+        int[] mySum = lc1480.runningSum(myArray);
+        System.out.println(mySum);
+    }
+
+}
