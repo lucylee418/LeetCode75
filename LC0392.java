4d42e3c1619757ced9504beed534d25ead90659a
Lucy Lee <55265883+lucylee418@users.noreply.github.com>
2023-01-20 23:21:22 -0500

    Updated

--- LC0392.java
+++ LC0392.java
@@ -38,7 +38,24 @@ class LC0392 {
         return last == s.length()-1;
     }
 
-    
+
+    // // Official Solution
+    // public boolean isSubsequence(String s, String t) {
+    //     Integer leftBound = s.length(), rightBound = t.length();
+    //     Integer pLeft = 0, pRight = 0;
+
+    //     while (pLeft < leftBound && pRight < rightBound) {
+    //         // move both pointers or just the right pointer
+    //         if (s.charAt(pLeft) == t.charAt(pRight)) {
+    //             pLeft += 1;
+    //         }
+    //         pRight += 1;
+    //     }
+    //     return pLeft == leftBound;
+    // }
+
+
+
     public static void main (String[] args){
         LC0392 lc0392 = new LC0392();
         String s = "abc";
