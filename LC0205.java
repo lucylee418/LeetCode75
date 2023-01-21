68f0f47bd3c50c0cdd9695a210096fd50009c2bc
Lucy Lee <55265883+lucylee418@users.noreply.github.com>
2023-01-20 22:01:31 -0500

    Updated

--- LC0205.java
+++ LC0205.java
@@ -1,6 +1,7 @@
 import java.util.*;
 
 public class LC0205 {
+    // Mine
     public boolean isIsomorphic(String s, String t) {
         // Compare the length
         if (s.length() != t.length()){
@@ -32,6 +33,38 @@ public class LC0205 {
         }
         return true;
     }
+
+
+    // // Official Solution
+    // public boolean isIsomorphic(String s, String t) {
+        
+    //             int[] mappingDictStoT = new int[256];
+    //             Arrays.fill(mappingDictStoT, -1);
+                
+    //             int[] mappingDictTtoS = new int[256];
+    //             Arrays.fill(mappingDictTtoS, -1);
+                
+    //             for (int i = 0; i < s.length(); ++i) {
+    //                 char c1 = s.charAt(i);
+    //                 char c2 = t.charAt(i);
+    //                 // System.out.println("c1: "+c1+", c2: "+c2);
+                    
+    //                 // Case 1: No mapping exists in either of the dictionaries
+    //                 if (mappingDictStoT[c1] == -1 && mappingDictTtoS[c2] == -1) {
+    //                     mappingDictStoT[c1] = c2;
+    //                     mappingDictTtoS[c2] = c1;
+    //                 }
+                    
+    //                 // Case 2: Ether mapping doesn't exist in one of the dictionaries or Mapping exists and
+    //                 // it doesn't match in either of the dictionaries or both 
+    //                 else if (!(mappingDictStoT[c1] == c2 && mappingDictTtoS[c2] == c1)) {
+    //                     // System.out.println("c1 el: "+ mappingDictStoT[c1]+ ", c2 el: "+mappingDictStoT[c2]);
+    //                     return false;
+    //                 }
+    //             }
+    //             return true;
+    //         }
+
 
 
     public static void main (String[] args){
